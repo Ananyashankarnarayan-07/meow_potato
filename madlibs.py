aeab5a70598bcfe30e59e08016ef47b32ecec753
ASB007 <ananyabhat0707l@gmail.com>
2025-10-27 18:18:21 +0530

    Add simple 10-line Python Mad Libs game

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# Mad Libs Game
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adj = input("Enter an adjective: ")
+place = input("Enter a place: ")
+food = input("Enter a food: ")
+
+print(f"One day, a {adj} {noun} decided to {verb} to {place}.")
+print(f"There, it found a giant plate of {food}!")
+print(f"It was so {adj} that everyone wanted some.")
+print(f"But the {noun} ran away shouting, 'This {food} is mine!'")
+print("And that's how the legend of the hungry traveler began.")
